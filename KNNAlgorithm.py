645bc857792f0752cf900bfd3f452024a585adf7
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-11-12 10:23:30 +0800

    KNN算法

--- KNNAlgorithm.py
+++ KNNAlgorithm.py
@@ -48,3 +48,64 @@ def classify0(inX, dataSet, labels, k):
 #          5）训练算法：此步骤不适用于k-近邻算法
 #          6）测试算法：使用海伦提供的部分数据作为测试样本
 #          7）使用算法：产生简单的命令行程序，然后海伦可以输入一些特征数据以判断对方是否为自己喜欢的类型
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros(numberOfLines, 3)
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index =+ 1
+
+    return returnMat.classLabelVector
+
+#    数值归一化
+#            在处理这种不同取值范围的特征值时，我们通常采用的方法是将数值归一化，如将取值范围处理为-到1或者-1
+#        到1之间，下面的共识将任意取值范围内的特征值转化为0到1区间内的值：
+#                    newValue = (oldValue - min/(max -min))
+#        其中min和max分别是数据集中的最小特征值和最大特征值。虽然改变数值取值范围增加了分类器的复杂度，但为了得到准确结果，必须这样做。
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    norDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = norDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+# 测试程序
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m.hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with ：%d, the real result is : %d"  % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
+
+#使用算法：构建完整可用系统
+    def classifyPerson():
+        resultList = ['not at all', 'in small doses', 'in large doses']
+        percentTats = float(raw_input("percentage of time spent playing video games?"))
+        ffMiles = float_(raw_input("frequent flier miles earned per year?"))
+        iceCream = float(raw_input("liters of ice cream consumed per year?"))
+        datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+        normMat, ranges, minVals = autoNorm(datingDataMat)
+        inArr = array([ffMiles, percentTats, iceCream])
+        classifierResult = classify0((inArr - minVals)/ ranges, normMat, datingLabels, 3)
+        print "You will probably like this person: " resultList[classifierResult - 1]
