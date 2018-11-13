f2c8238b59441cd6f820c66e470ea648a8e06b97
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-11-13 18:33:35 +0800

    朴素贝叶斯，贝叶斯算法

---
+++ BayesAlgorithm.py
@@ -0,0 +1,86 @@
+#coding=utf-8
+
+#    要从文本中获取特征，需要先拆分文本，适用词条向量标识磁条有没有出现在词条向量中，0表示词条未出现，1表示词条出现过
+#
+
+from numpy import *
+
+def loadDataSet():
+    postingList = {
+        ['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+        ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+        ['my', 'dalmation', 'is', 'so', 'cute', 'i','love', 'him'],
+        ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+        ['mr', 'licks', 'ate', 'my', 'steak', 'how','to','stop','him'],
+        ['quit','buying','worthless','dog','fond','stupid']}
+
+    classVec = [0,1,0,1,0,1]
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+#   训练算法：从词向量计算概率
+#
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory/float(numTrainDocs))
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = p1Num/p1Denom
+    p0Vect = p0Num/p0Denom
+    return p0Vect, p1Vect,pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass)
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat=[]
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB((thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print testEntry, 'classified as: ', classifyNB((thisDoc, p0V, p1V, pAb))
+
+#文档词袋模型
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in inputSet:
+            returnVec[vocabList.index[word]] += 1
+    return returnVec
