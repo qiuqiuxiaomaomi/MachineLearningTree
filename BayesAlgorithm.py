4d5f26fc4fc9de7ec9165b40260e9383f3043eeb
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-11-14 22:26:37 +0800

    贝叶斯算法研究

--- BayesAlgorithm.py
+++ BayesAlgorithm.py
@@ -4,6 +4,7 @@
 #
 
 from numpy import *
+import re
 
 def loadDataSet():
     postingList = {
@@ -84,3 +85,72 @@ def bagOfWords2VecMN(vocabList, inputSet):
         if word in inputSet:
             returnVec[vocabList.index[word]] += 1
     return returnVec
+
+def textParse(bigString):
+    listOfTokens = re.split(r'\W+', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList=[]
+    classList=[]
+    fullText=[]
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet=[]
+    for i in range(10):
+        randIndex=int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat=[]
+    trainClasses=[]
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam=trainNB0(array(trainMat), array(trainClasses))
+    errorCount=0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB[array(wordVector), p0V, p1V, pSpam] != classList[docIndex]:
+            errorCount += 1
+    print  'the error rate is : ', float(errorCount) / len(testSet)
+
+def calcMostFreq(vocabList, fullText):
+    freqDict = {}
+    for token in vocabList:
+        freqDict[token] = fullText.count(token)
+    sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedFreq
+
+def localWords(feed1, feed0):
+    docList=[]
+    classList=[]
+    fullText=[]
+    minLen = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+
+
+def getTopWords(ny, sf):
+    vocabList, p0V, p1V = localWords(ny, sf)
+    topNY=[]
+    topSF=[]
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0:
+            topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] > -6.0:
+            topNY.append((vocabList[i], p1V[i]))
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    for item in sortedSF:
+        print  item[0]
+    sortedNY=sorted(topNY, key=lambda pair:pair[1], reverse=True)
+    for item in sortedNY:
+        print item[0]
